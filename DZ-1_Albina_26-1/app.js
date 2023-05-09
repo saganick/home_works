9d4a5bbaaebedffe295ea8f696252a2093cf6c6c
dibi <133033039+saganick@users.noreply.github.com>
2023-05-09 22:06:44 +0600

    выполнила дз-1

---
+++ DZ-1_Albina_26-1/app.js
@@ -0,0 +1,51 @@
+var rate = prompt("Оставьте свой отзыв от 1 до 10");
+
+
+switch (rate) {
+    case "1":
+        console.log("Мы работаем над улучшением сервиса!");
+        break;
+    case "2":
+        console.log("Мы работаем над улучшением сервиса!");
+        break;
+    case "3":
+        console.log("Мы работаем над улучшением сервиса!");
+        break;
+    case "4":
+        console.log("Мы работаем над улучшением сервиса!");
+        break;
+    case "5":
+        console.log("Спасибо вам за высокую оценку!");
+        break;
+    case "6":
+        console.log("Спасибо вам за высокую оценку!");
+        break;
+    case "7":
+        console.log("Спасибо вам за высокую оценку!");
+        break;
+    case "8":
+        console.log("Спасибо вам за высокую оценку!");
+        break;
+    case "9":
+        console.log("Спасибо вам за высокую оценку!");
+        break;
+    case "10":
+        console.log("Спасибо вам за высокую оценку!");
+        break;
+    default:
+        console.log("ERROR!!");
+        break;
+}
+// var lang = prompt('"en"- Дни недели на английском языке' +
+//     ' "ru"- Дни недели на русском языке')
+// switch (lang) {
+//     case 'ru':
+//         console.log(['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'])
+//         break
+//     case 'en':
+//         console.log(['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'])
+//         break
+//     default:
+//         alert('ERROR')
+//         break
+// }
