30a39bff03f0446578f9d3076c0aba40824685f7
dibi <133033039+saganick@users.noreply.github.com>
2023-05-10 02:13:13 +0600

    выполнила дз-2

---
+++ DZ-2_Albina_26-1/main.js
@@ -0,0 +1,65 @@
+//1)Пункт :
+
+// var arr = [10,20,30,50,239,3000,5045]
+//
+// for (var item of arr=[10,20,239,50,5045]) {
+//     console.log(item)
+// }
+//
+
+
+//2) Пункт:
+//
+// var arr = [20,15,10,5,3,0]
+//
+// for (var item of arr) {
+//     console.log(item)
+// }
+
+
+
+//3)Пункт:
+
+      // if else
+//
+// var lights = prompt('введи цвета цветофора')
+//
+// if (lights ='красный' ){
+//     alert("стоп!")
+// }
+// else if ( 'желтый') {
+//     alert('жди!')
+// }
+// else if ( 'зеленый') {
+//     alert('иди!')
+// }
+
+   // switch case
+// var rate = prompt('нужно вести цвет сфетофора')
+//
+// switch (rate) {
+//     case "красный":
+//         alert('стоп!')
+//         break
+//     case 'желтый':
+//         alert('жди!')
+//         break
+//     case 'зеленый':
+//         alert('иди!')
+//         break
+//     default:
+//         alert('вести только цвета светофора !!!')
+//         break
+// }
+
+
+//4) Пункт
+
+// var caunt = 0
+// arr = []
+//
+// while (caunt <= 10) {
+//     console.log(caunt)
+//     caunt++
+//
+// }
