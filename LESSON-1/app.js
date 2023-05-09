063cf95190c9f78911d41b46b53c35db90b1c0e7
dibi <133033039+saganick@users.noreply.github.com>
2023-05-09 22:13:10 +0600

    my less

---
+++ LESSON-1/app.js
@@ -0,0 +1,53 @@
+//console.log("Hello World")
+
+
+// var books = '1000 heror'
+// var name = 'Albina'
+// console.log(book , name)
+
+
+// типы данных (6 примитивных )
+
+// var name = 'Albina'
+// var age = 18
+// var isMarried = false
+//     //
+// var isMarriedage = true
+//
+// var money = undefined
+// var brain = null  //empty
+// var notnum = 12 + '2'
+
+//console.log(notnum)
+
+//
+// var arr = [123,    'я' ,'albina' ]
+// //var name = arr[2]
+//
+// console.log(arr[2])
+
+
+
+
+
+
+// var human = {
+//    name:  'Oleg',
+//     age:  20 ,
+//     array: [1,2,3,]
+// }
+
+
+
+
+
+//console.log (human.name)
+
+if ( age > 18 )
+
+
+
+
+
+
+
