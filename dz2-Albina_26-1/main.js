8686ed495d5738421dc07b78826aabc7621eeb71
dibi <133033039+saganick@users.noreply.github.com>
2023-05-11 14:02:45 +0600

    Выполнила дз-2

---
+++ dz2-Albina_26-1/main.js
@@ -0,0 +1,63 @@
+//1- Пункт
+
+// var arr = [10,20,30,50,230,300,540]
+//
+// for (var num of arr) {
+//     var number = num.toString();
+//     if (number.startsWith("1") || number.startsWith('2') || number.startsWith('5')){
+//         console.log(num)
+//     }
+//
+// }
+
+
+//2-Пункт
+
+// for (var i = 20; i >= 0; i--) {
+//     console.log(i)
+// }
+
+
+//3-Пункт
+
+// var color = prompt('Введите цвета светофора!')
+//
+// if (color === 'красный'){
+//     alert("стоп!")
+// } else if (color === 'желтый'){
+//     alert('жди!')
+// } else if (color === 'зеленый'){
+//     alert('иди!')
+// } else {
+//     alert('введите только цвет!')
+// }
+
+   // 3 ПУНКТ--SWITCH CASE
+
+//var color = prompt('Введите цвета светофора!')
+// switch (color){
+//     case "red":
+//         alert("stop!");
+//         break;
+//     case "yellow":
+//         alert("wait!");
+//         break;
+//     case "green":
+//         alert("go!");
+//         break;
+//     default:
+//         alert('error')
+//         break
+// }
+
+
+
+//4 Пункт
+//
+// var caunt=0
+// var arr = []
+// while (caunt<=10 ){
+//     arr.push(caunt);
+//     caunt++
+// }
+// console.log(arr)
